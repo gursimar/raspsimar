8a27073033b0f4aea1e982a2dcaa8d6a9f85038c
simar <simar.i3r@gmail.com>
2016-03-03 06:30:30 +0530

    integrated sqlalchemy and flask-restless into flask

--- webapp/views.py
+++ webapp/views.py
@@ -1,16 +1,41 @@
+from flask.ext.restless.manager import APIManager
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import INTEGER, Text
+
 from webapp import app
 from flask import render_template
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///system.db'
+db = SQLAlchemy(app)
+
+class Video(db.Model):
+    id= Column(INTEGER, primary_key=True)
+    title = Column(Text, unique=False)
+    image = Column(Text, unique=False)
+db.create_all()
+
+api_manager = APIManager(app, flask_sqlalchemy_db=db)
+api_manager.create_api(Video, methods = ['GET', 'POST', 'DELETE', 'PUT'])
+
+
 @app.route('/')
 def index():
+    return app.send_static_file('index.html')
+
+# this is jinja example
+@app.route('/jinja')
+def jinja():
     name = 'simar'
-    return render_template('index.html', name=name)
-    
+    return render_template('jinja.html', name=name)
+
+# just a basic page
 @app.route('/cakes')
 def cakes():
     return 'cakes'
 
-@app.route('/welcome')
+#bootstrap example
+@app.route('/bootstrap')
 def welcome():
-	return render_template('welcome.html')
+	return render_template('bootstrap.html')
 
